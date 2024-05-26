7b04e0d19463be8e42ecfa108e839893b3eb349c
s <ghub@test1.qazlatin.kz>
2024-05-26 10:13:41 +0500

    v vetke main izmena 3 sec na 4 sec commit

--- kiris_create.php
+++ kiris_create.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Перенаправление через 3 секунды</title>
-    <meta http-equiv="refresh" content="3;url=index.php">
+    <title>Перенаправление через 4 секунды</title>
+    <meta http-equiv="refresh" content="4;url=index.php">
 </head>
 <body>
     <p>База әлі істелмеді</p>
-    <p>3 сек-тан соң басқы бетке ораласыз.</p>
+    <p>4 сек-тан соң басқы бетке ораласыз.</p>
 </body>
 </html>
